e22d3a7a946c6417a18151418aeed615bb6b43e8
Yoav Nordmann <Yoav.Nordmann@Gmail.com>
2016-04-11 12:04:28 +0300

    Adding Unit Tests

---
+++ src/test/java/com/hp/opta/flex/configuration/model/MetaDataFactoryTest.java
@@ -0,0 +1,34 @@
+package com.hp.opta.flex.configuration.model;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MetaDataFactoryTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCreateTokenMetaData() {
+		TokenMetaData test = MetaDataFactory.createTokenMetaData(
+				"testName", "Integer", "testFormat", 1, 1, 1);
+		Assert.assertNotNull("The Object must not be null", test);
+		Assert.assertEquals(1, test.getIndex());
+		Assert.assertEquals("testName",test.getName());
+		Assert.assertEquals("testFormat",test.getFormat());
+		Assert.assertEquals(TokenType.Integer,test.getType());
+		
+		
+		test = MetaDataFactory.createTokenMetaData(
+				"testName", "Integer", "testFormat", 1, 2, 3);
+		Assert.assertNull("The Object should be null", test);		
+	}
+
+}
